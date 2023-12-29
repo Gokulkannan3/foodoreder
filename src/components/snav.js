0b48eee96e55229f9b66af2e77675015af7fbc6c
Gokul Kannan P <91548753+Gokulkannan3@users.noreply.github.com>
2023-12-29 22:31:08 +0530

    Update snav.js

--- src/components/snav.js
+++ src/components/snav.js
@@ -1,30 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import innum from '../images/innum.png'
 import '../App.css'
 import { Link } from "react-router-dom";
-import './snav.css'
+import './lnav.css';
+import menu from '../images/menu.png'
+import close from '../images/remove.png'
 
-export default function Snav(){
-    return(
-        <div className='out bg-black h-32'>
+export default function Snav() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    const closeMenu = () => {
+        setMenuOpen(false);
+    };
+
+    return (
+        <nav className='out bg-black h-32'>
             <div className="flex justify-start translate-y-3 translate-x-5">
-                <img src={innum} alt="innum" className="w-24"/>
-            </div>         
+                <img src={innum} alt="innum" className="w-24" />
+            </div>
             <div className="head font-bold text-white translate-x-36 -translate-y-16 text-2xl">
                 <p>Innum Vai!!!</p>
                 <p>Happy Tummy Ahead!!!</p>
             </div>
-            <div className="flex items-center justify-end -translate-y-32 -translate-x-5 gap-4">
-                <Link to='/'>
-                    <button className="text-white font-bold text-xl h-12 w-20 rounded-lg bg-red-500 hover:bg-red-400 hover:text-black">Home</button>
-                </Link>
-                <Link to='/about'>
-                    <button className="text-white font-bold text-xl h-12 w-20 rounded-lg bg-red-500 hover:bg-red-400 hover:text-black">About</button>
-                </Link>
-                <Link to='/login'>
-                    <button className="text-white font-bold text-xl h-12 w-20 rounded-lg bg-red-500 hover:bg-red-400 hover:text-black">Login</button>
-                </Link>
+            <div className="flex justify-end -translate-y-28">
+                <div className={`menu cursor-pointer w-14 flex justify-end ${menuOpen ? 'menu-open' : ''}`} onClick={toggleMenu}>
+                    <img src={menu} alt="menu" className="w-8" />
+                </div>
             </div>
-        </div>
+            <div className="flex justify-end -translate-y-40">
+                <div className="bg-black w-52 items-center-translate-y-40 -mt-7">
+                    <ul className={menuOpen ? "open" : ":"}>
+                        <div className="w-6 cursor-pointer translate-x-40 mt-2" onClick={closeMenu}>
+                            <img src={close} alt="Close"/>
+                        </div>
+                        <li>
+                            <Link to='/'>
+                                <button className="text-white font-bold mb-4 text-xl h-12 w-20 rounded-lg bg-red-500 hover:bg-red-400 hover:text-black">Home</button>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/login'>
+                                <button className="text-white font-bold mb-4 text-xl h-12 w-20 rounded-lg bg-red-500 hover:bg-red-400 hover:text-black">Login</button>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     )
 }
