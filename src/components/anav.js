cb1f7177f8a9c60aa374decafedb8a4be2f5c7b9
Gokul Kannan P <91548753+Gokulkannan3@users.noreply.github.com>
2023-12-29 22:21:07 +0530

    Update anav.js

--- src/components/anav.js
+++ src/components/anav.js
@@ -4,29 +4,46 @@ import '../App.css'
 import { Link } from "react-router-dom";
 import './lnav.css';
 import menu from '../images/menu.png'
+import close from '../images/remove.png'
 
-export default function Anav(){
+export default function Anav() {
     const [menuOpen, setMenuOpen] = useState(false);
-    return(
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    const closeMenu = () => {
+        setMenuOpen(false);
+    };
+
+    return (
         <nav className='out bg-black h-32'>
             <div className="flex justify-start translate-y-3 translate-x-5">
-                <img src={innum} alt="innum" className="w-24"/>
-            </div>         
+                <img src={innum} alt="innum" className="w-24" />
+            </div>
             <div className="head font-bold text-white translate-x-36 -translate-y-16 text-2xl">
                 <p>Innum Vai!!!</p>
                 <p>Happy Tummy Ahead!!!</p>
             </div>
-            <div className={`menu w-14 translate-x-80 ml-6 -translate-y-28 ${menuOpen ? 'menu-open' : ''}`} onClick={() => setMenuOpen(!menuOpen)}>
-                <img src={menu} alt="menu" className="w-8" />
+            <div className="flex justify-end -translate-y-28">
+                <div className={`menu cursor-pointer w-14 flex justify-end ${menuOpen ? 'menu-open' : ''}`} onClick={toggleMenu}>
+                    <img src={menu} alt="menu" className="w-8" />
+                </div>
             </div>
-            <div className="flex bg-black w-52 items-center justify-end -translate-y-20 -mt-2.5 translate-x-52 gap-y-4">
-                <ul className={menuOpen ? "open" : ":"}>
-                    <li>
-                        <Link to='/alogin'>
-                            <button className="text-white font-bold mb-4 text-xl h-12 w-20 rounded-lg bg-red-500 hover:bg-red-400 hover:text-black">Logout</button>
-                        </Link>
-                    </li>
-                </ul>
+            <div className="flex justify-end -translate-y-40">
+                <div className="bg-black w-52 items-center-translate-y-40 -mt-7">
+                    <ul className={menuOpen ? "open" : ":"}>
+                        <div className="w-6 cursor-pointer translate-x-40 mt-2" onClick={closeMenu}>
+                            <img src={close} alt="Close"/>
+                        </div>
+                        <li>
+                            <Link to='/asignin'>
+                                <button className="text-white font-bold mb-4 text-xl h-12 w-20 rounded-lg bg-red-500 hover:bg-red-400 hover:text-black">Signin</button>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     )
