1d5e50c0d8b0c5656cb4cd51045d5d317bb96a39
Gokul Kannan P <91548753+Gokulkannan3@users.noreply.github.com>
2023-12-29 20:42:41 +0530

    Update anav.js

--- src/components/anav.js
+++ src/components/anav.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import innum from '../images/innum.png'
 import '../App.css'
 import { Link } from "react-router-dom";
 import './lnav.css';
+import menu from '../images/menu.png'
 
 export default function Anav(){
+    const [menuOpen, setMenuOpen] = useState(false);
     return(
-        <div className='out bg-black h-32'>
+        <nav className='out bg-black h-32'>
             <div className="flex justify-start translate-y-3 translate-x-5">
                 <img src={innum} alt="innum" className="w-24"/>
             </div>         
@@ -14,17 +16,18 @@ export default function Anav(){
                 <p>Innum Vai!!!</p>
                 <p>Happy Tummy Ahead!!!</p>
             </div>
-            <div className="flex items-center justify-end -translate-y-32 -translate-x-5 gap-4">
-                <Link to='/home'>
-                    <button className="text-white font-bold text-xl h-12 w-20 rounded-lg bg-red-500 hover:bg-red-400 hover:text-black">Home</button>
-                </Link>
-                <Link to='/about'>
-                    <button className="text-white font-bold text-xl h-12 w-20 rounded-lg bg-red-500 hover:bg-red-400 hover:text-black">About</button>
-                </Link>
-                <Link to='/'>
-                    <button className="text-white font-bold text-xl h-12 w-20 rounded-lg bg-red-500 hover:bg-red-400 hover:text-black">Logout</button>
-                </Link>
+            <div className={`menu w-14 translate-x-80 ml-6 -translate-y-28 ${menuOpen ? 'menu-open' : ''}`} onClick={() => setMenuOpen(!menuOpen)}>
+                <img src={menu} alt="menu" className="w-8" />
             </div>
-        </div>
+            <div className="flex bg-black w-52 items-center justify-end -translate-y-20 -mt-2.5 translate-x-52 gap-y-4">
+                <ul className={menuOpen ? "open" : ":"}>
+                    <li>
+                        <Link to='/alogin'>
+                            <button className="text-white font-bold mb-4 text-xl h-12 w-20 rounded-lg bg-red-500 hover:bg-red-400 hover:text-black">Logout</button>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     )
 }
